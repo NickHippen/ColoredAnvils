d419208bdc16653aa4d52374bc32ef61bcd1b0e3
Nick Hippen <nhippen973@gmail.com>
2021-09-18 23:29:37 -0500

    Generate clean plugin

---
+++ src/main/java/me/flamingkatana/mc/plugins/coloredanvils/ColoredAnvils.java
@@ -0,0 +1,17 @@
+package me.flamingkatana.mc.plugins.coloredanvils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ColoredAnvils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
